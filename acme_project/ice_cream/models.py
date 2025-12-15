638aa7c5aa7c34f163ccf7b763966f64b8311e4f
Evgenii Kolomeets <kolomeetsevgenii@yandex.ru>
2025-12-15 13:59:17 +1000

    f5

--- acme_project/ice_cream/models.py
+++ acme_project/ice_cream/models.py
@@ -1,27 +1,24 @@
 from django.db import models
 from django.db.models import PositiveSmallIntegerField
+from core.models import PublishedModel
 
 
-class Wrapper(models.Model):
+class Wrapper(PublishedModel, models.Model):
     title = models.CharField(max_length=256)
-    is_published = models.BooleanField(default=True)
 
 
-class Topping(models.Model):
+class Topping(PublishedModel, models.Model):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
-    is_published = models.BooleanField(default=True)
 
 
-class Category(models.Model):
+class Category(PublishedModel, models.Model):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
     output_order = PositiveSmallIntegerField(default=100)
-    is_published = models.BooleanField(default=True)
 
 
-class IceCream(models.Model):
-    is_published = models.BooleanField(default=True)
+class IceCream(PublishedModel, models.Model):
     is_on_main = models.BooleanField(default=False)
     title = models.CharField(max_length=256)
     description = models.TextField()
